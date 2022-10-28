7b248404d792e1f07eb6857dcc43c38204c906cc
Matthew Hausman <mattadamhausman@gmail.com>
2022-10-28 15:52:57 -0400

    update babel config

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  presets: ['@babel/preset-typescript', '@babel/preset-react'],
+  presets: ['@babel/preset-typescript'],
 
   overrides: [
     {
-      include: ['./packages/core', './packages/react'],
+      include: ['./packages/core', './packages/vue'],
       presets: [['@babel/preset-env', { targets: 'defaults, not ie 11' }]],
     },
   ],
