7f16d3959f233dcb404b278e4e9243c5684e0bb5
Matthew Hausman <matthewhausman7@gmail.com>
2022-11-02 17:36:44 -0400

    problems with polyfills on test app

--- examples/vue-example/vite.config.ts
+++ examples/vue-example/vite.config.ts
@@ -4,17 +4,13 @@ import nodePolyfills from 'rollup-plugin-polyfill-node'
 
 import { URL, fileURLToPath } from 'node:url'
 
-const MODE = process.env.NODE_ENV
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    MODE === 'development'
-      ? nodePolyfills({
+    nodePolyfills({
           include: ['node_modules/**/*.js', new RegExp('node_modules/.vite/.*js')]
         })
-      : null
   ],
   resolve: {
     alias: {
