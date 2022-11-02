309ac09c5402e6b9c663b29457e3c5466290a82d
Matthew Hausman <matthewhausman7@gmail.com>
2022-11-02 17:19:47 -0400

    updates

--- examples/vue-example/vite.config.ts
+++ examples/vue-example/vite.config.ts
@@ -1,15 +1,35 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import nodePolyfills from 'rollup-plugin-polyfill-node'
 
 import { URL, fileURLToPath } from 'node:url'
 
+const MODE = process.env.NODE_ENV
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    MODE === 'development'
+      ? nodePolyfills({
+          include: ['node_modules/**/*.js', new RegExp('node_modules/.vite/.*js')]
+        })
+      : null
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        // ↓ Needed for build
+        nodePolyfills()
+      ]
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true
     }
   }
 })
