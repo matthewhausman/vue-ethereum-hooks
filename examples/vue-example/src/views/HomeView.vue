9a77e656c703d0c8cd2bc17c8a4ea0b11b1d1f5a
Matthew Hausman <matthewhausman7@gmail.com>
2023-02-09 00:52:07 -0500

    update style

--- examples/vue-example/src/views/HomeView.vue
+++ examples/vue-example/src/views/HomeView.vue
@@ -1,24 +1,31 @@
 <script setup lang="ts">
 import { useConnect } from '@vue-ethereum-hooks/hooks';
-import TheWelcome from '../components/TheWelcome.vue'
 const { connect, connectors, error, isLoading, pendingConnector } =
     useConnect()
 </script>
 
 <template>
-  <main>
-    <div :style="{display: 'flex', flexDirection: 'column'}">
+  <main :style="{width: '100%', height: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}">
+    <div :style="{display: 'flex', flexDirection: 'column', gap: '10px', minWidth: '400px'}">
       <button
         v-for="connector in connectors"
         :disabled="!connector.ready"
         :key="connector.id"
+        :style="{
+          background: '#333',
+          border: '1px solid grey',
+          padding: '8px',
+          borderRadius: '4px',
+          cursor: 'pointer',
+          color: 'white'
+        }"
         @click="() => connect({ connector })"
       >
         <p v-if="connector.name">{{ connector.name }}</p>
         <p v-if="!connector.ready">{{ ' (unsupported)' }}</p>
         <p v-if="isLoading && (pendingConnector?.id === connector.id)">{{ ' (connecting)' }}</p>
       </button>
-      <div>{{error?.message}}</div>
     </div>
+    <div :style="{marginTop: '10px'}">{{error?.message}}</div>
   </main>
 </template>
